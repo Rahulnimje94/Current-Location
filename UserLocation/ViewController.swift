9a19fe7ca51e9af166c3710561bc35452ce1a540
Anand <anand@anands-mac-pro.local>
2017-12-12 19:10:38 +0530

    Added User's Current Location Functionality.

--- UserLocation/ViewController.swift
+++ UserLocation/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var Mapkit: MKMapView!
+    
+    let manager = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        
+        let span: MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        let mylocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        
+        let region: MKCoordinateRegion = MKCoordinateRegionMake(mylocation, span)
+        Mapkit.setRegion(region, animated: true)
+        
+        annotation(location)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+        }
+    
+    func annotation(_ locations: CLLocation) {
+        let annotation = MKPointAnnotation()
+        let centerCoordinate = CLLocationCoordinate2D(latitude: locations.coordinate.latitude, longitude: locations.coordinate.longitude)
+        annotation.coordinate = centerCoordinate
+        annotation.title = "Title"
+        Mapkit.addAnnotation(annotation)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
